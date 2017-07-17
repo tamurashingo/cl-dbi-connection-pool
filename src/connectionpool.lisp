59fb115db7c5d67befa300ace607f7f1b1968b1f
tamura shingo <tamura.shingo@gmail.com>
2017-07-17 12:43:44 +0900

    add proxy class

--- src/connectionpool.lisp
+++ src/connectionpool.lisp
@@ -20,20 +20,52 @@
 (defvar *CONNECTION-HASH* NIL
   "dbi-connection to connection array index")
 
+
 @export
-(defun make-connection-pool (driver-name &rest params &key database-name username password (initial-size 10) (max-size 10))
+(defclass <dbi-connection-pool> ()
+  ((pool :type array
+         :initarg :pool
+         :documentation "array of <pooled-connection>")
+   (connect-fn :type function
+               :initarg :connect-fn
+               :documentation "function what connecto to database ")))
+
+(defclass <pooled-connnection> ()
+  ((connect-p :type boolean
+              :accessor connect-p
+              :initform NIL)
+   (semaphore :accessor semaphore
+              :initform (bt-sem:make-semaphore :count 1))
+   (dbi-connection-proxy :type <dbi-connection-proxy>
+                         :initarg :dbi-connection-proxy
+                         :accessor dbi-connection-proxy
+                         :initform NIL)))
+
+@export
+(defun make-connection-pool (driver-name &rest params &key database-name username passwod (initial-size 10) (max-size 10))
   "make connection pool
 
 Example
   (make-connection-pool :mysql :database-name \"cldbi\" :username \"root\" :password \"password\")"
-  (disconnect-all)
-  (setf *CONNECTIONS* (make-array initial-size :adjustable T))
-  (setf *CONNECTION-HASH* (make-hash-table :test #'eq))
-  (loop for index from 0 below initial-size
-        ; do (format T "setting ~A~%" index)
-        do (let* ((conn (apply #'dbi:connect driver-name params))
-                  (sem (make-dbi-semaphore :connection conn)))
-             (setf (aref *CONNECTIONS* index) sem))))
+  (let ((pool (make-array max-size :initial-element NIL))
+        (dbi-connection-pool
+         (make-instance '<dbi-connection-pool>
+                        :pool pool
+                        :connect-fn (lambda ()
+                                      (apply #'dbi:connect driver-name params)))))
+    ;; create <pooled-connection> ]instance
+    (loop for idx from 0 below max-size
+          do (setf (aref pool idx)
+                   (make-instance '<pooled-connection>)))
+    ;; connect initial-size connection
+    (let ((connect-fn (connect-fn dbi-connection-pool)))
+      (loop for idx from 0 below initial-size
+            do (let ((pooled-conn (aref pool idx)))
+                 (setf (connect-p pooled-conn) T)
+                 (setf (dbi-connection-proxy pooled-conn) (funcall connect-fn)))))
+
+    dbi-connection-pool))
+
 
 @export
 (defun disconnect-all ()
---
+++ src/proxy.lisp
@@ -0,0 +1,42 @@
+(in-package :cl-user)
+(defpackage dbi-cp.proxy
+  (:use :cl
+        :cl-annot
+        :cl-dbi))
+(in-package :dbi-cp.proxy)
+
+(cl-syntax:use-syntax :annot)
+
+
+(defannotation proxy (def-form)
+    (:arity 1)
+  "just a mark"
+  def-form)
+
+@export
+@export-accessors
+(defclass <dbi-connection-proxy> ()
+  ((dbi-connection :type <dbi-connection>
+                   :initarg :dbi-connection
+                   :accessor dbi-connection)
+   (disconnect-fn :type function
+                  :initarg :disconnect-fn
+                  :accessor disconnect-fn)))
+
+@proxy
+(defmethod disconnect ((conn <dbi-cnnection-proxy>))
+  (let ((dbi-connection (dbi-connection conn))
+        (disconnect-fn (disconnect-fn conn)))
+    (rollback dbi-connection)
+    (funcall disconnect-fn)))
+
+@proxy
+(defmethod prepare ((conn <dbi-connection-proxy>) (sql string) &rest rest &key &allow-other-keys)
+  (let ((dbi-connection (dbi-connection conn)))
+    (apply #'prepare dbi-connection sql rest)))
+
+@proxy
+(defmethod do-sql ((conn <dbi-connection-proxy>) (sql string) &rest params)
+  (let ((dbi-connection (dbi-connection conn)))
+    (apply #'do-sql dbi-connection sql params)))
+
