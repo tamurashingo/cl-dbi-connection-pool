0582eb40a36dda724d5fc3234f73947f9adb91c0
tamura shingo <tamura.shingo@gmail.com>
2020-05-18 22:52:35 +0900

    mod execute parameters

--- t/proxy.lisp
+++ t/proxy.lisp
@@ -13,13 +13,13 @@
 ;; ----------------------------------------
 
 (defun test-do-sql (conn)
-  (is (do-sql conn "DROP TABLE IF EXISTS person") nil)
+  (is (do-sql conn "DROP TABLE IF EXISTS person") 0)
   (is (do-sql conn "CREATE TABLE person (id INTEGER PRIMARY KEY, name VARCHAR(24) NOT NULL)")
-      nil)
+      0)
   (is (do-sql conn "INSERT INTO person (id, name) values (1, 'fukamachi')")
-      nil)
+      1)
   (is (do-sql conn "INSERT INTO person (id, name) values (2, 'matsuyama')")
-      nil))
+      1))
 
 (defun test-prepare-execute-fetch (conn)
   (let (query result)
@@ -43,7 +43,7 @@
   (is (row-count conn) 1)
 
   (let* ((query (prepare conn "SELECT * FROM person WHERE name = ?"))
-         (result (execute query "snmsts")))
+         (result (execute query '("snmsts"))))
     (is (getf (fetch result) :|name|) "snmsts")))
 
 (defun test-with-transaction (conn)
